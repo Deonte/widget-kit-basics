49fe9a9aadb15d2aea42835e898a446e0aa41d4e
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-12-13 23:56:45 -0500

    Initial setup.

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Deonte Kilgore on 12/13/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
