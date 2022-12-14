df9761408cb4f2bf39e95bdc52ca0f74fa7c8c02
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-12-13 23:53:25 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Deonte Kilgore on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
