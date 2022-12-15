0060b10b33e1d1e3ac8d3066acd559b9db126a39
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-12-14 23:16:37 -0500

    Created styles for each month of the year.

--- MonthlyWidget/MonthlyWidget.swift
+++ MonthlyWidget/MonthlyWidget.swift
@@ -9,23 +9,24 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [DayEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        // Generate a timeline consisting of 7 entries a day apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+        for dayOffset in 0 ..< 7 {
+            let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+            let startOfDate = Calendar.current.startOfDay(for: entryDate)
+            let entry = DayEntry(date: startOfDate)
             entries.append(entry)
         }
 
@@ -34,15 +35,53 @@ struct Provider: TimelineProvider {
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct DayEntry: TimelineEntry {
     let date: Date
 }
 
 struct MonthlyWidgetEntryView : View {
-    var entry: Provider.Entry
-
+    var entry: DayEntry
+    var config: MonthConfig
+    
+    init(entry: DayEntry) {
+        self.entry = entry
+        self.config = MonthConfig.determineConfig(from: entry.date)
+    }
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack {
+            ContainerRelativeShape()
+                .fill(config.backgroundColor.gradient)
+            
+            VStack {
+                HStack(spacing: 4) {
+                    Text(config.emojiText)
+                        .font(.title)
+                    Text(entry.date.weekdayDisplayFormat)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .minimumScaleFactor(0.6)
+                        .foregroundColor(config.weekdayTextColor)
+                    
+                    Spacer()
+                }
+                
+                Text(entry.date.dayDisplayFormat)
+                    .font(.system(size: 80, weight: .heavy))
+                    .foregroundColor(config.dayTextColor)
+            }
+            .padding()
+        }
+    }
+}
+
+extension Date {
+    var weekdayDisplayFormat: String {
+        self.formatted(.dateTime.weekday(.wide))
+    }
+    
+    var dayDisplayFormat: String {
+        self.formatted(.dateTime.day())
     }
 }
 
@@ -53,14 +92,21 @@ struct MonthlyWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             MonthlyWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Monthly Style Widget")
+        .description("The theme of the widget changes based on the month.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
 struct MonthlyWidget_Previews: PreviewProvider {
     static var previews: some View {
-        MonthlyWidgetEntryView(entry: SimpleEntry(date: Date()))
+        MonthlyWidgetEntryView(entry: DayEntry(date: dateToDisplay(month: 5, day: 23)))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+    
+    static func dateToDisplay(month: Int, day: Int) -> Date {
+        let components = DateComponents(calendar: Calendar.current, year:  2022, month: month, day: day)
+        
+        return Calendar.current.date(from: components)!
     }
 }
